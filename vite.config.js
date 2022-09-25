783b9b28d1ae753456511f94047f329cb9b176c7
Fayzulla <fayzullarakhmatullayev@gmail.com>
2022-09-26 00:18:43 +0500

    init: app

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from "vite";
+export default defineConfig({
+  base: "https://fayzullarakhmatullayev.github.io/online-zoo",
+});
+
+// git subtree push --prefix dist origin gh-pages
