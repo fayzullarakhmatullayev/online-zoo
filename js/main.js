66b59b0e02994589d8ac9be02c8b5f5fa3832db2
Fayzulla <fayzullarakhmatullayev@gmail.com>
2022-10-01 15:48:53 +0500

    feature: add gallery and donation sections

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 console.log("success");
+
+new Swiper(".gallery_swiper", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  spaceBetween: 30,
+});
